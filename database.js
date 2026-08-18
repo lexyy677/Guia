6596fb327b580662bc70acaf6ede2d6487713cdb
lexyy677 <nkidsixseve6@gmail.com>
2026-08-18 01:04:56 -0600

    Create database.js

---
+++ database.js
@@ -0,0 +1,23 @@
+// Base de datos de cuentas predeterminadas del Staff
+// Puedes añadir o editar las personas que quieras en esta lista:
+
+const usersDatabase = [
+  {
+    Nombre: "Alexis",
+    Nick: "Lexyy677",
+    Correo: "lexyy@sirenmc.net",
+    Contrasena: "Lexyy677"
+  },
+  {
+    Nombre: "Juan Pérez",
+    Nick: "JuanStaff",
+    Correo: "juan@sirenmc.net",
+    Contrasena: "JuanStaff"
+  },
+  {
+    Nombre: "Carlos",
+    Nick: "CarlitosMC",
+    Correo: "carlos@sirenmc.net",
+    Contrasena: "CarlitosMC"
+  }
+];
